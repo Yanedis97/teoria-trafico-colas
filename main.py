e210c371c134614a9888624e3bfdcc43e8a2618d
Yanedis Maza <yanedismaza@gmail.com>
2024-04-21 18:58:39 -0500

    add url in cors

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5000", "localhost:3000",
+                   "localhost:5173","http://127.0.0.1:5173",
+                   "http://localhost:5173", "http://localhost:5173/",
                    "http://localhost:3000", "http://localhost:3000/",
                    "https://trafico-y-cola.vercel.app/", 
                    "https://trafico-y-cola.vercel.app"],
